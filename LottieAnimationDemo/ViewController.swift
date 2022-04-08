a3be4427d86e04be66e9b84afa6b84033e02c446
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-04-08 12:07:35 +0300

    Setup animation

--- LottieAnimationDemo/ViewController.swift
+++ LottieAnimationDemo/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
+
+    var animationView = AnimationView()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        setUpAnimation()
     }
 
 
+}
+
+extension ViewController {
+    private func setUpAnimation() {
+        animationView.animation = Animation.named("pusculite")
+        animationView.frame = view.bounds
+        animationView.backgroundColor = .red
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        view.addSubview(animationView)
+    }
 }
 
